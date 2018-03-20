800c4e23c6ec68aacc38ab88fe369894c84decb1
AndreyOOP <andrew_yudaichev@i.ua>
2018-03-20 17:11:56 +0200

    Add ParseService  - constructor  - NavigateToSite  - Testing of NavigateToSite

---
+++ CarParser0/Parser/ParseService.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium.IE;
+using System;
+
+namespace CarParser0.ParserNS
+{
+    public class ParseService
+    {
+        InternetExplorerDriver Driver;
+
+        public ParseService(InternetExplorerDriver Driver)
+        {
+            this.Driver = Driver;
+        }
+
+        public Boolean NavigateToSite(String url, Func<Boolean> isLoaded)
+        {
+            Driver.Navigate().GoToUrl(url);
+
+            return isLoaded();
+        }
+    }
+}
---
+++ CarParser0Tests/Tests/Parser/ParseServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarParser0.ParserNS;
+using System;
+using OpenQA.Selenium.IE;
+
+namespace CarParser0.SiteParser.Tests
+{
+    [TestClass()]
+    public class ParseServiceTests
+    {
+        InternetExplorerDriver Driver;
+        ParseService Service;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            Driver = new InternetExplorerDriver("Resources/");
+
+            Service = new ParseService(Driver);
+        }
+
+        [TestMethod()]
+        public void NavigateToSiteSuccessTest()
+        {
+            String Url = "http://localhost:49242/ParseService/NavigatetoUrl/MD619865.htm";
+
+            Func<Boolean> isCorrectPage = () =>
+            {
+                var elem = Driver.FindElementsById("ak_rev");
+
+                return elem.Count > 0;
+            };
+
+            Assert.IsTrue( Service.NavigateToSite(Url, isCorrectPage));
+        }
+
+        [TestMethod()]
+        public void NavigateToWrongSiteTest()
+        {
+            String Url = "http://localhost:49242/error.htm";
+
+            Func<Boolean> isCorrectPage = () =>
+            {
+                var elem = Driver.FindElementsById("ak_rev");
+
+                return elem.Count > 0;
+            };
+
+            Assert.IsFalse( Service.NavigateToSite(Url, isCorrectPage));
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Driver.Quit();
+        }
+    }
+}
